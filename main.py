e875d1b0307c80d49d220e9137abc7d3ce4d6161
JustinV879 <72768893+JustinV879@users.noreply.github.com>
2021-01-21 13:22:25 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+while True: 
+    print("Temperature (F): " + input.temperature(TemperatureUnit.FAHRENHEIT) and print("Light Level:" + input.light_level()))
+    if (input.light_level() and input.temperature(TemperatureUnit.FAHRENHEIT)) >= 40:
+        music.pew_pew.play()
+        music.set_volume(300)
